f80fc2e36fe5f57538bfc68878d55f84526d69ad
anrotar <Andrei.Rotar@endava.com>
2016-08-01 14:25:27 +0300

    Trainings

---
+++ TrainMeNowMVC/TrainMeNowDAL/DAL_classes/TrainingsDAL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainMeNowDAL.DAL_classes
+{
+    class TrainingsDAL
+    {
+        public Training GetByID(int id)
+        {
+            using(var ctx = new Internship2016NetTrainMeNowEntities())
+            {
+                return ctx.Trainings.Find(id);
+            }
+        }
+
+        public void Delete(Training training)
+        {
+            using(var ctx = new Internship2016NetTrainMeNowEntities())
+            {
+                ctx.Trainings.Remove(training);
+            }
+        }
+
+        public IEnumerable<Training> GetAllTrainings()
+        {
+            using (var ctx = new Internship2016NetTrainMeNowEntities())
+            {
+                return ctx.Trainings.ToList();
+            }
+        }
+    }
+}
