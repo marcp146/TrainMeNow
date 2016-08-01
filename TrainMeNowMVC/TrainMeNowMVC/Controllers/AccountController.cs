c12af2440666f7a62f378fd2869ec6e9fde858ce
bchetan <bchetan@EN63500>
2016-08-01 15:09:36 +0300

    Login Form

--- TrainMeNowMVC/TrainMeNowMVC/Controllers/AccountController.cs
+++ TrainMeNowMVC/TrainMeNowMVC/Controllers/AccountController.cs
@@ -13,5 +13,10 @@ namespace TrainMeNowMVC.Controllers
         {
             return View();
         }
+        public ActionResult Login()
+        {
+            
+            return View();
+        }
     }
 }
