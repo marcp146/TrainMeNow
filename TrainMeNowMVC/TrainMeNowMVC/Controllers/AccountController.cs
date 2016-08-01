19385ea6583df6e145044001c0037253447462c1
dacarp <daniel.carp@endava.com>
2016-08-01 14:47:12 +0300

    register2

--- TrainMeNowMVC/TrainMeNowMVC/Controllers/AccountController.cs
+++ TrainMeNowMVC/TrainMeNowMVC/Controllers/AccountController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TrainMeNowMVC.Models;
+using TrainMeNowDAL;
+using System.Security.Cryptography;
 
 namespace TrainMeNowMVC.Controllers
 {
@@ -11,6 +14,23 @@ namespace TrainMeNowMVC.Controllers
         // GET: Account
         public ActionResult Index()
         {
+            return View();
+        }
+
+        public ActionResult Register(UserViewModel model)
+        {
+            using(var ctx = new Internship2016NetTrainMeNowEntities())
+            {
+                var user = new User();
+                user.Email = model.Email;
+                user.FirstName = model.FirstName;
+                user.LastName = model.LastName;
+                user.Password = model.Password;
+                user.RoleId = 3;
+            }
+
+
+
             return View();
         }
     }
