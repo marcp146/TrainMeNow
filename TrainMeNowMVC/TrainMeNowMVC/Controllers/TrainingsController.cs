5f1f5bf53f951ea2f12af80f932b9f45c40afc33
anrotar <Andrei.Rotar@endava.com>
2016-08-02 15:31:05 +0300

    Change

--- TrainMeNowMVC/TrainMeNowMVC/Controllers/TrainingsController.cs
+++ TrainMeNowMVC/TrainMeNowMVC/Controllers/TrainingsController.cs
@@ -1,44 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using TrainMeNowDAL;
-using TrainMeNowMVC.Models;
-
-namespace TrainMeNowMVC.Controllers
-{
-    public class TrainingsController : Controller
-    {
-        // GET: Trainings
-        public ActionResult TrainingsListByTrainerId(int? id)
-        {
-            var trainingsList = new TrainingsDal().getTrainings(id).Select(x => new TrainingViewModel { Id = x.Id, Name = x.Name, TrainerId = x.TrainerId, Price = x.Price, MaxUsers = x.MaxUsers }).ToList();
-            return View(trainingsList);
-        }
-
-        public ActionResult MyTrainings()
-        {
-            if (Session["User"] != null)
-            {
-                List<OrdersViewModel> orderslist = new List<OrdersViewModel>();
-                using (var ctx = new Internship2016NetTrainMeNowEntities())
-                {
-                    var user = ctx.Users.Find((int)Session["User"]);
-                    var myTrainingList = user.Orders;
-                    foreach (Order ord in myTrainingList)
-                    {
-                        orderslist.Add(new OrdersViewModel { Id = ord.ID, Name = ord.Training.Name, Trainer = ord.Training.User.FirstName + " " + ord.Training.User.LastName });
-                    }
-                }
-                return View(orderslist);
-            }
-            else
-            {
-                return RedirectToAction("Login", "Account");
-            }
-        }
-    }
+﻿
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -83,5 +43,30 @@ namespace TrainMeNowMVC.Controllers
 
             }
         }
+
+            public ActionResult MyTrainings()
+        {
+            if (Session["User"] != null)
+            {
+                List<OrdersViewModel> orderslist = new List<OrdersViewModel>();
+                using (var ctx = new Internship2016NetTrainMeNowEntities())
+                {
+                    var user = ctx.Users.Find((int)Session["User"]);
+                    var myTrainingList = user.Orders;
+                    foreach (Order ord in myTrainingList)
+                    {
+                        orderslist.Add(new OrdersViewModel { Id = ord.ID, Name = ord.Training.Name, Trainer = ord.Training.User.FirstName + " " + ord.Training.User.LastName });
+                    }
+                }
+                return View(orderslist);
+            }
+            else
+            {
+                return RedirectToAction("Login", "Account");
+            }
+        }
+
     }
+
+
 }
