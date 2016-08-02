c248747ae4784cbb68df9013d029b7c44da7d789
dacarp <daniel.carp@endava.com>
2016-08-02 15:16:20 +0300

    Admin Controller

--- TrainMeNowMVC/TrainMeNowMVC/Controllers/AdminController.cs
+++ TrainMeNowMVC/TrainMeNowMVC/Controllers/AdminController.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using TrainMeNowMVC.Models;
+using TrainMeNowDAL;
 namespace TrainMeNowMVC.Controllers
 {
     public class AdminController : Controller
@@ -16,9 +17,18 @@ namespace TrainMeNowMVC.Controllers
 
         public ActionResult TrainerList()
         {
-
-
-            return View();
+            var trainers = new List<UserViewModel>();
+            foreach (var user in UsersDAL.getUsersByRole(2))
+            {
+                trainers.Add(new UserViewModel
+                {
+                    Email = user.Email,
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    Username = user.Username
+                });
+            }
+            return View(trainers);
         }
     }
 }
--- TrainMeNowMVC/TrainMeNowMVC/Models/UserViewModel.cs
+++ TrainMeNowMVC/TrainMeNowMVC/Models/UserViewModel.cs
@@ -8,13 +8,12 @@ namespace TrainMeNowMVC.Models
 {
     public class UserViewModel
     {
-        [Key]
         public int Id { get; set; }
         public string Username { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public int RoleId { get; set; }
+       // public int RoleId { get; set; }
     }
 }
