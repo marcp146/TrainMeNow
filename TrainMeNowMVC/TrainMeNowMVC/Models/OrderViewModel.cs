a7fa8de6663b1c581752fab2c52d8f4ea3056a40
RaulMerlan <Raul.Merlan@endava.com>
2016-08-02 13:42:30 +0300

    OrderViewModel

---
+++ TrainMeNowMVC/TrainMeNowMVC/Models/OrderViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrainMeNowMVC.Models
+{
+    public class OrderViewModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int TrainingId { get; set; }
+        public int PaymentId { get; set; }
+    }
+}
