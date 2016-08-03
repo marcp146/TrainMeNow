53681cf645e2bb4ce2c7c4aa3d870db41f96e46d
dacarp <daniel.carp@endava.com>
2016-08-03 13:50:39 +0300

    xsdfdghfj

--- TrainMeNowMVC/TrainMeNowMVC/Controllers/AdminController.cs
+++ TrainMeNowMVC/TrainMeNowMVC/Controllers/AdminController.cs
@@ -39,6 +39,49 @@ namespace TrainMeNowMVC.Controllers
 
 
         }
+
+        public ActionResult ManageTrainers()
+        {
+            if (Session["User"] != null && (int)Session["RoleId"] == 1)
+            {
+                var users = new List<User>();
+                var dal = new UsersDAL();
+                var model = new List<UserViewModel>();
+
+                var trainerId = 2;
+                var userId = 3;
+
+                users = UsersDAL.getUsersByRole(trainerId);
+                users.AddRange(UsersDAL.getUsersByRole(userId));
+
+                foreach (var u in users)
+                {
+                    model.Add(new UserViewModel
+                    {
+                        Username = u.Username,
+                        Id = u.RoleId,
+                        Email = u.Email,
+                        FirstName = u.FirstName,
+                        LastName = u.LastName
+                    });
+                }
+                return View(model);
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult ManageTrainers(List<UserViewModel> model)
+        {
+
+
+
+            return View();
+        }
+
         public ActionResult TrainingList()
         {
             var trainingList = new TrainingsDal().getAllTrainings().Select(x => new TrainingViewModel { Id = x.Id, Name = x.Name, TrainerId = x.TrainerId, Price = x.Price, MaxUsers = x.MaxUsers }).ToList();
