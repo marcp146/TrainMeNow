66887ec2391acc60ff985a10dc366c7ebccfa5ac
Raul.Merlan <Raul.Merlan@endava.com>
2016-08-04 11:30:13 +0300

    TrainerController - Update TrainingsListByTrainerId - new button "ListTrainees"

--- TrainMeNowMVC/TrainMeNowMVC/Controllers/TrainerController.cs
+++ TrainMeNowMVC/TrainMeNowMVC/Controllers/TrainerController.cs
@@ -18,44 +18,42 @@ namespace TrainMeNowMVC.Controllers
             return View();
         }
 
-        public ActionResult ListTrainees()
+        public ActionResult ListTrainees(int currentTrainingID)
         {
             using (var ctx = new Internship2016NetTrainMeNowEntities())
             {
                 if (Session["RoleId"] != null && (int)Session["RoleId"] == 2)
                 {
-                    int currentTrainerID = (int)Session["User"];
-                    var trainingsList = new List<TrainingViewModel>();
-                    var trainingsDal = new TrainingsDal();
-                    foreach (var training in trainingsDal.getTrainingsByTrainerId(currentTrainerID))
-                    {
-                        trainingsList.Add(new TrainingViewModel
-                        {
-                            Id = training.Id,
-                            Name = training.Name,
-                            TrainerId = training.TrainerId,
-                            Price = training.Price,
-                            MaxUsers = training.MaxUsers
-                        });
-                    }
+                    //int currentTrainerID = (int)Session["User"];
+                    //var trainingsList = new List<TrainingViewModel>();
+                    //var trainingsDal = new TrainingsDal();
+                    //foreach (var training in trainingsDal.getTrainingsByTrainerId(currentTrainerID))
+                    //{
+                    //    trainingsList.Add(new TrainingViewModel
+                    //    {
+                    //        Id = training.Id,
+                    //        Name = training.Name,
+                    //        TrainerId = training.TrainerId,
+                    //        Price = training.Price,
+                    //        MaxUsers = training.MaxUsers
+                    //    });
+                    //}
 
 
                     List<OrderViewModel> ordersList = new List<OrderViewModel>();
                     var ordersDal = new OrdersDAL();
-                    foreach (var tr in trainingsList)
-                    {
-                        foreach (var order in ordersDal.GetOrdersByTrainingID(tr.Id))
-                        {
-                            ordersList.Add(new OrderViewModel
-                            {
-                                Id = order.ID,
-                                UserId = order.UserID,
-                                TrainingId = order.TrainingID,
-                                PaymentId = order.PaymentID
-                            });
-                        }
-                    }
 
+                    foreach (var order in ordersDal.GetOrdersByTrainingID(currentTrainingID))
+                    {
+                        ordersList.Add(new OrderViewModel
+                        {
+                            Id = order.ID,
+                            UserId = order.UserID,
+                            TrainingId = order.TrainingID,
+                            PaymentId = order.PaymentID
+                        });
+                    }
+                    
                     List<UserViewModel> usersList = new List<UserViewModel>();
                     var usersDal = new UsersDAL();
                     foreach (var or in ordersList)
