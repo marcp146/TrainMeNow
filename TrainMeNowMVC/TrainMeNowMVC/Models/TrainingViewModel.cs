f87401edb26852cb5c9c0e4ea31650c8d1498897
dacarp <daniel.carp@endava.com>
2016-08-04 11:19:32 +0300

    EDMX pray

--- TrainMeNowMVC/TrainMeNowMVC/Models/TrainingViewModel.cs
+++ TrainMeNowMVC/TrainMeNowMVC/Models/TrainingViewModel.cs
@@ -11,8 +11,8 @@ namespace TrainMeNowMVC.Models
         [Key]
         public int Id { get; set; } 
         public string Name { get; set; } 
-        public int? TrainerId { get; set; } 
-        public decimal? Price { get; set; } 
-        public int? MaxUsers { get; set; }
+        public int TrainerId { get; set; } 
+        public decimal Price { get; set; } 
+        public int MaxUsers { get; set; }
     }
 }
